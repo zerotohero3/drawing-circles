70aaeae1d18b3ac3c79f1a1ab0594480bcb67474
Victor Hugo <113398568+zerotohero3@users.noreply.github.com>
2023-08-31 20:27:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,26 +6,48 @@ canvas.height = window.innerHeight;
 
 let isDrawing = false;
 
-canvas.addEventListener('mousedown', () => {
+function startDrawing(event) {
     isDrawing = true;
-});
+    draw(event.touches[0].clientX, event.touches[0].clientY);
+    event.preventDefault();
+}
 
-canvas.addEventListener('mouseup', () => {
+function stopDrawing() {
     isDrawing = false;
     context.beginPath();
-});
+}
 
-canvas.addEventListener('mousemove', (event) => {
+function draw(x, y) {
     if (!isDrawing) return;
 
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
+    const isMobile = window.innerWidth <= 768;
+
+    const radius = isMobile ? 15 : 35;
 
     context.beginPath();
-    context.arc(mouseX, mouseY, 35, 0, Math.PI * 2);
+    context.arc(x, y, radius, 0, Math.PI * 2);
     context.strokeStyle = 'white';
-    context.fillStyle = 'black'; 
+    context.fillStyle = 'black';
     context.lineWidth = 1;
     context.fill();
     context.stroke();
+}
+
+canvas.addEventListener('touchstart', startDrawing);
+canvas.addEventListener('touchmove', (event) => {
+    draw(event.touches[0].clientX, event.touches[0].clientY);
+});
+canvas.addEventListener('touchend', stopDrawing);
+
+canvas.addEventListener('mousedown', () => {
+    isDrawing = true;
+});
+canvas.addEventListener('mousemove', (event) => {
+    draw(event.clientX, event.clientY);
+});
+canvas.addEventListener('mouseup', stopDrawing);
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 });
